60bbf7ae38abd294aac2c84a384e895653c146ec
emanuelkirui0 <emanuelkirui044@gmail.com>
2026-04-03 20:23:54 +0000

    Simplify API client to match requested format

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,42 +1,7 @@
-import axios, { AxiosInstance, AxiosError, AxiosResponse } from 'axios'
-import Cookies from 'js-cookie'
+import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL
+const API = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+});
 
-const API: AxiosInstance = axios.create({
-  baseURL,
-  withCredentials: true,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-})
-
-// Request interceptor to add token to headers
-API.interceptors.request.use(
-  (config) => {
-    const token = Cookies.get('token')
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`
-    }
-    return config
-  },
-  (error) => {
-    return Promise.reject(error)
-  }
-)
-
-// Response interceptor to handle 401 errors
-API.interceptors.response.use(
-  (response) => response,
-  (error: AxiosError) => {
-    if (error.response?.status === 401) {
-      Cookies.remove('token')
-      if (typeof window !== 'undefined') {
-        window.location.href = '/login'
-      }
-    }
-    return Promise.reject(error)
-  }
-)
-
-export default API
+export default API;
