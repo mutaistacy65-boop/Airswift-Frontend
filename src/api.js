f2899fadb07de352a6fdd3f41ae2e2cdee33bdcc
martinjalopy-cyber <martinjalopy@gmail.com>
2026-04-04 11:36:59 +0000

    Add API URL logging to api.js

--- src/api.js
+++ src/api.js
@@ -1,3 +1,7 @@
+const API = process.env.NEXT_PUBLIC_API_URL;
+
+console.log("API URL:", API);
+
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://airswift-backend-pdk2.onrender.com";
 
 export default API_BASE_URL;
